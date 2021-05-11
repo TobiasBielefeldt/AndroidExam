c8c55323e1a08156985488079d58ae27888e2968
Tobias B <tobie18@student.sdu.dk>
2021-05-11 09:30:22 +0200

    Origin

--- WaterApp/app/src/main/java/com/example/waterapp/repositories/PlantRepository.kt
+++ WaterApp/app/src/main/java/com/example/waterapp/repositories/PlantRepository.kt
@@ -5,11 +5,12 @@ import com.example.waterapp.database.Plant
 class PlantRepository()
 {
     private val plantDatabase: MutableList<Plant> = mutableListOf()
+    //In a perfect world this would be in a database and then the first time you use the app it would download it
     init{
-        insert(Plant.createNewPlant("Cat-Plant", "This is a cat, not a plant",1,4))
-        insert(Plant.createNewPlant("Croton", "The croton is an easy-to-grow houseplant known for its variegated foliage covered in green, scarlet, orange, and yellow splotches.",4,4))
-        insert(Plant.createNewPlant("Orchid" ,"The beauty, complexity and incredible diversity of orchid flowers are unrivalled in the plant world. These exotic beauties comprise the largest family of flowering plants on earth, with over 30,000 different species, and at least 200,000 hybrids." , 4,2))
-        insert(Plant.createNewPlant("Cactus", "Cacti are some of the most unusual and elegant plants in the world, with bold shapes of all kinds and beautiful green colour variations." , 5,2))
+        insert(Plant.createNewPlant("Cat-Plant", "Origin: Egypt. This is a cat, not a plant",1,4))
+        insert(Plant.createNewPlant("Croton", "Origin: South-East Asia. The croton is an easy-to-grow houseplant known for its variegated foliage covered in green, scarlet, orange, and yellow splotches.",4,4))
+        insert(Plant.createNewPlant("Orchid" ,"Origin: Earth. The beauty, complexity and incredible diversity of orchid flowers are unrivalled in the plant world." , 4,2))
+        insert(Plant.createNewPlant("Cactus", "Origin: Desert. Cacti are some of the most unusual and elegant plants in the world, with bold shapes of all kinds and beautiful green colour variations." , 5,2))
     }
 
     //This Repository works the same way as the PersonalPlantRepository so look at that if you need help
