7f4f7caa4df278dfb7eddee1250112d28874d5ed
Tobias B <tobie18@student.sdu.dk>
2021-05-12 16:17:53 +0200

    Disable buttons if the user is already on the fragment

--- WaterApp/app/src/main/java/com/example/waterapp/MainActivity.kt
+++ WaterApp/app/src/main/java/com/example/waterapp/MainActivity.kt
@@ -39,18 +39,24 @@ class MainActivity : AppCompatActivity() {
 
         //Button initialization
         val btnHome = findViewById<Button>(R.id.btnHome)
-        btnHome.setOnClickListener { updateFragment(HomeFragment()) }
+        btnHome.setOnClickListener { updateFragment(HomeFragment(),"Home") }
 
         val btnSearch = findViewById<Button>(R.id.btnSearch)
-        btnSearch.setOnClickListener{updateFragment(SearchFragment())}
+        btnSearch.setOnClickListener{updateFragment(SearchFragment(),"Search")}
     }
 
     //Simple fragment update function
-    private fun updateFragment(currentFragment: Fragment) {
-        supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.main_fragment, currentFragment)
-                .addToBackStack(null)
-                .commit()
+    private fun updateFragment(currentFragment: Fragment,tag: String) {
+        var fragment = supportFragmentManager.findFragmentByTag(tag)
+
+        //If this is false then the we are on the selected fragment so no reason to create a new one
+        if(!(fragment != null && fragment.isVisible))
+        {
+            supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.main_fragment, currentFragment,tag)
+                    .addToBackStack(null)
+                    .commit()
+        }
     }
 }
